46025c55eff58293c3ae44115afc31a243096f55
1lolplayer1 <126320304+1lolplayer1@users.noreply.github.com>
2023-06-28 16:59:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function change(){
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-    document.getElementsByClassName("title")[0].style.transform = "scale(0.01)";
-    document.getElementsByClassName("title")[0].style.transition = "1s";
-    sleep(1000).then(() => {
-        document.getElementsByClassName("title")[0].style.display = "none";
-    });
-}
 function changePath() {
     location.href = 'https://mck.survivo.tk/index.html'
 }
